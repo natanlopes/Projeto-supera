7c59086934274010d1ff9a8597b7b1925a9c6a81
Natan Lopes <natan1918@gmail.com>
2023-07-13 10:29:49 -0300

    add commint comentarios sobre o codigo e codigo atualizado

--- src/segundo_desafio/SegundoDesafio.java
+++ src/segundo_desafio/SegundoDesafio.java
@@ -4,6 +4,13 @@
 package segundo_desafio;
 
 /**
+ * O código fornecido realiza o seguinte:
+ * Lê um valor de ponto flutuante da entrada usando o Scanner.
+ * Define uma lista de notas disponíveis (100, 50, 20, 10, 5, 2) e uma lista de moedas disponíveis (1, 0.5, 0.25, 0.10, 0.05, 0.01).
+ * Calcula a quantidade mínima de notas necessárias para trocar o valor inicial. Ele percorre a lista de notas, dividindo o valor atual pelo valor da nota e armazenando a quantidade de notas necessárias. Em seguida, atualiza o valor para o resto da divisão.
+   Imprime a quantidade de notas para cada valor na lista de notas.
+   Calcula a quantidade mínima de moedas necessárias da mesma maneira que as notas.
+   Imprime a quantidade de moedas para cada valor na lista de moedas.
  * 
  */
 import java.util.Scanner;
@@ -14,48 +21,22 @@ public class SegundoDesafio {
 
         double valor = scanner.nextDouble(); // Lê um valor de ponto flutuante da entrada
 
-        // Calcula a quantidade de notas de cada valor
-        int notas100 = (int) (valor / 100);
-        valor %= 100;
-        int notas50 = (int) (valor / 50);
-        valor %= 50;
-        int notas20 = (int) (valor / 20);
-        valor %= 20;
-        int notas10 = (int) (valor / 10);
-        valor %= 10;
-        int notas5 = (int) (valor / 5);
-        valor %= 5;
-        int notas2 = (int) (valor / 2);
-        valor %= 2;
+        int[] notas = { 100, 50, 20, 10, 5, 2 }; // Notas disponíveis
+        double[] moedas = { 1, 0.5, 0.25, 0.10, 0.05, 0.01 }; // Moedas disponíveis
 
-        // Calcula a quantidade de moedas de cada valor
-        int moedas1 = (int) (valor / 1);
-        valor %= 1;
-        int moedas050 = (int) (valor / 0.50);
-        valor %= 0.50;
-        int moedas025 = (int) (valor / 0.25);
-        valor %= 0.25;
-        int moedas010 = (int) (valor / 0.10);
-        valor %= 0.10;
-        int moedas005 = (int) (valor / 0.05);
-        valor %= 0.05;
-        int moedas001 = (int) (valor / 0.01);
-
-        // Imprime a quantidade de notas e moedas necessárias
         System.out.println("NOTAS:");
-        System.out.println(notas100 + " nota(s) de R$ 100.00");
-        System.out.println(notas50 + " nota(s) de R$ 50.00");
-        System.out.println(notas20 + " nota(s) de R$ 20.00");
-        System.out.println(notas10 + " nota(s) de R$ 10.00");
-        System.out.println(notas5 + " nota(s) de R$ 5.00");
-        System.out.println(notas2 + " nota(s) de R$ 2.00");
+        for (int i = 0; i < notas.length; i++) {
+            int quantidade = (int) (valor / notas[i]);
+            valor %= notas[i];
+            System.out.println(quantidade + " nota(s) de R$ " + notas[i] + ".00");
+        }
+
         System.out.println("MOEDAS:");
-        System.out.println(moedas1 + " moeda(s) de R$ 1.00");
-        System.out.println(moedas050 + " moeda(s) de R$ 0.50");
-        System.out.println(moedas025 + " moeda(s) de R$ 0.25");
-        System.out.println(moedas010 + " moeda(s) de R$ 0.10");
-        System.out.println(moedas005 + " moeda(s) de R$ 0.05");
-        System.out.println(moedas001 + " moeda(s) de R$ 0.01");
+        for (int i = 0; i < moedas.length; i++) {
+            int quantidade = (int) (valor / moedas[i]);
+            valor %= moedas[i];
+            System.out.println(quantidade + " moeda(s) de R$ " + String.format("%.2f", moedas[i]));
+        }
 
         scanner.close(); // Fechar o Scanner
     }
